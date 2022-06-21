2e7e10f06464cc27a3827ac476011891372d99fc
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-06-21 18:29:16 -0300

    Criado a função para validação da idade do palestrante

--- talkerValidation.js
+++ talkerValidation.js
@@ -14,6 +14,23 @@ function nameValidation(req, res, next) {
   next();
 }
 
+function ageValidation(req, res, next) {
+  const { age } = req.body;
+
+  if (!age) {
+    return res.status(400)
+      .json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (age < 18) {
+    return res.status(400)
+      .json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+}
+
 module.exports = {
   nameValidation,
+  ageValidation,
 };
