21f333c2255b11d015600bbfe63651988d7e6633
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-06-21 19:01:24 -0300

    Criado a função para validação do campo talk

--- talkerValidation.js
+++ talkerValidation.js
@@ -30,7 +30,50 @@ function ageValidation(req, res, next) {
   next();
 }
 
+function watchDateValidation(date, res) {
+  const validate = /\d{2}\/\d{2}\/\d{4}/g;
+
+  if (!date) {
+    return res.status(400)
+      .json({ message: 'message": "O campo "watchedAt" é obrigatório' });
+  }
+
+  if (!validate.test(date)) {
+    return res.status(400)
+      .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+}
+
+function rateValidation(rate, res) {
+  if (!rate) {
+    return res.status(400)
+      .json({ message: 'O campo "rate" é obrigatório' });
+  }
+
+  if (rate >= 1 && rate <= 5) {
+    return res.status(400)
+      .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+}
+
+function talkValidation(req, res, next) {
+  const { talk } = req.body;
+  
+  if (!talk) {
+    return res.status(400)
+      .json({ message: 'O campo "talk" é obrigatório' });
+  }
+  
+  const { watchedAt, rate } = talk;
+
+  watchDateValidation(watchedAt, res);
+  rateValidation(rate, res);
+
+  next();
+}
+
 module.exports = {
   nameValidation,
   ageValidation,
+  talkValidation,
 };
