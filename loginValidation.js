b533f7eb3ee110845305ff70fbb641f4264cf4f7
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-06-21 16:12:53 -0300

    criando a validação de password

--- loginValidation.js
+++ loginValidation.js
@@ -1,4 +1,4 @@
-function emailValidation(req, res) {
+function emailValidation(req, res, next) {
   const { email } = req.body;
   // site onde verifiquei o regex para validação do email https://regexr.com/
   const validation = /\b[\w.-]+@[\w.-]+\.\w{2,4}\b/;
@@ -12,8 +12,27 @@ function emailValidation(req, res) {
     return res.status(400)
       .json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
+
+  next();
+}
+
+function passwordValidation(req, res, next) {
+  const { password } = req.body;
+  
+  if (!password) {
+    return res.status(400)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.lenghth < 6) {
+    return res.status(400)
+      .json({ message: 'O  "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
 }
 
 module.exports = {
   emailValidation,
+  passwordValidation,
 };
